2369ecaef71939c3380169f16aa8476a4ec36532
jiwon <jwpark@wert.co.kt>
2023-09-26 16:45:12 +0900

    test

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebsocketDemoApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketDemoApplication.class, args);
 	}
